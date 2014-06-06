2119f8183e65f29fdc861f5de1211425f364ba98
anirudh <anirudh@scatterbot.com>
2014-06-07 03:33:27 +0530

    added controller Article and created post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Blog::Application.routes.draw do
+R2::Application.routes.draw do
  
   resources :articles
  
@@ -9,7 +9,7 @@ end
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+#root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -59,4 +59,4 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
