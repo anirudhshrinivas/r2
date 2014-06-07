10023c4703869ec935d1ad878ba9b318dd4b68b7
anirudh <anirudh@scatterbot.com>
2014-06-07 10:21:05 +0530

    Added basic validation with Models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,11 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 
-		@article.save 
-		redirect_to @article
+		if @article.save
+    		redirect_to @article
+  		else
+    		render 'new'
+  		end
 	end
 	
 	def show  
@@ -21,6 +24,4 @@ class ArticlesController < ApplicationController
 	def article_params
 		params.require(:article).permit(:title, :text)
 	end
-
-
 end
