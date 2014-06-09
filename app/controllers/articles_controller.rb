afad0b6b4bf94e4c5271a90acbb43d20b7c585bc
anirudh <anirudhshrinivas@hotmail.com>
2014-06-09 18:16:16 +0530

    added test method destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
   		end
 	end
 	
+	def destroy 
+	end
+
+	
 	def show  
 		@article = Article.find(params[:id])
 	end
@@ -27,6 +31,28 @@ class ArticlesController < ApplicationController
 	end
 end
 
+=begin
 def new
   
 end
+=end
+
+=begin
+def new
+  @article = Article.new
+end
+ 
+def create
+  @article = Article.new(article_params)
+ 
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end=end
